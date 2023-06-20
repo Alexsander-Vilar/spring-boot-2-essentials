ada4e1ac99f61101eb751fcbab78bea9086972c6
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-06-20 10:08:33 -0300

    criação do domain

---
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -0,0 +1,12 @@
+package academy.devdojo.springboot2essentials.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Anime {
+    private long id;
+    private String anime;
+
+}
