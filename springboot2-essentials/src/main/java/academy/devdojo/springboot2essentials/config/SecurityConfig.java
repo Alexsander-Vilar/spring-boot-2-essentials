b6f17b60cd70801b7d899250dc5deab5a9535f83
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-06-29 14:18:18 -0300

    Spring Security CSRF token

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -9,9 +9,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static org.hibernate.boot.model.process.spi.MetadataBuildingProcess.build;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
