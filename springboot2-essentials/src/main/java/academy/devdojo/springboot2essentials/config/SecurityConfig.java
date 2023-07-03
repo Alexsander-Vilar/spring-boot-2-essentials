6f7ff01880ee8847a030761c5a8eecdbe49ca479
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-07-03 10:05:25 -0300

    Spring Boot Actuator

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/animes/admin/**").hasRole("ADMIN")
                         .requestMatchers("/animes/**").hasRole("USER")
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults());
